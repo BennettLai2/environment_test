2cec9473b2dadeafc6b9d569cba81c4fb40c50ca
BennettLai2 <bxl190020@tamu.edu>
2022-09-15 20:14:27 -0500

    fix integrate test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -15,4 +15,42 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content(42.0)
     expect(page).to have_content(Date.new(2022,9,8))
   end
+
+  scenario 'no name' do 
+    visit new_book_path
+    fill_in 'Author', with: 'J.K. Rolling'
+    fill_in 'Price', with: 42.0
+    fill_in 'Date', with: Date.new(2022,9,8)
+    click_on 'Create Book'
+    expect(page).to have_content("Title can't be blank")
+  end
+
+  scenario 'no author' do 
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Price', with: 42.0
+    fill_in 'Date', with: Date.new(2022,9,8)
+    click_on 'Create Book'
+    expect(page).to have_content("Author can't be blank")
+  end
+
+  scenario 'no price' do 
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K. Rolling'
+    fill_in 'Price', with: 42.0
+    fill_in 'Date', with: Date.new(2022,9,8)
+    click_on 'Create Book'
+    expect(page).to have_content("Price can't be blank")
+  end
+
+  scenario 'np date' do 
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K. Rolling'
+    fill_in 'Price', with: 42.0
+    fill_in 'Date', with: Date.new(2022,9,8)
+    click_on 'Create Book'
+    expect(page).to have_content("Date can't be blank")
+  end
 end
